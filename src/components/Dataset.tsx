a32d6c735d26226a8b06c05c097a59f70fa951dd
lightest <lightest13@gmail.com>
2024-05-29 15:56:03 +0100

    removed old code.

--- src/components/Dataset.tsx
+++ src/components/Dataset.tsx
@@ -1,67 +1,6 @@
 import dataset from "../../data/PubChemElements_all.json";
 import { iChemElement } from "../types/ChemElement";
 
-const planets = [
-	{
-		id: 0, planet: "voxtron", type: "vulcanic",
-		radius: 1,
-		surfaceArea: 2,
-		mass: 3,
-	},
-
-	{
-		id: 1, planet: "tatooin", type: "desert",
-		radius: 2,
-		surfaceArea: 2,
-		mass: 3,
-	},
-
-	{
-		id: 2, planet: "hoth", type: "ice",
-		radius: 3,
-		surfaceArea: 2,
-		mass: 3,
-	},
-
-	{
-		id: 3, planet: "kashyk", type: "forest",
-		radius: 4,
-		surfaceArea: 2,
-		mass: 3,
-	},
-
-	{
-		id: 4, planet: "manaan", type: "ocean",
-		radius: 5,
-		surfaceArea: 2,
-		mass: 3,
-	},
-
-	{
-		id: 5, planet: "endor", type: "jungle",
-		radius: 6,
-		surfaceArea: 2,
-		mass: 3,
-	}
-];
-
-// const chemicalElements = [
-// 	{
-// 		name: "hydrogen",
-// 		atomicNumber: 1,
-// 		atomicMass: 1.0080,
-// 		symbol: "H",
-// 		group: "halogen"
-// 	},
-// 	{
-// 		name: "Copper",
-// 		atomicNumber: 29,
-// 		atomicMass: 63.55,
-// 		symbol: "Cu",
-// 		group: "transition metal"
-// 	},
-// ];
-
 const COLUMN_NAMES_REMAP = {
 	"AtomicNumber": "atomicNumber",
 	"Symbol": "symbol",
@@ -138,7 +77,6 @@ const {
 console.log(chemicalElementsLUT);
 
 export {
-	planets,
 	chemicalElementsList,
 	chemicalElementsLUT
 };
