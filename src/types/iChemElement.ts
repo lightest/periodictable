e3836b3e4a695fe4934b7b7ea471db11eb626ef4
lightest <lightest13@gmail.com>
2024-06-09 23:44:26 +0100

    ChemicalElement class started.

--- src/types/iChemElement.ts
+++ src/types/iChemElement.ts
@@ -8,3 +8,25 @@ export interface iChemElement
 	name: string,
 	hexColor: string
 }
+
+export class ChemicalElement
+{
+	atomicNumber: number;
+	atomicMass: number;
+	electronConfiguration: string;
+	symbol: string;
+	group: string;
+	name: string;
+	hexColor: string;
+
+	constructor(data: iChemElement)
+	{
+		this.atomicNumber = data.atomicNumber;
+		this.atomicMass = data.atomicMass;
+		this.electronConfiguration = data.electronConfiguration;
+		this.symbol = data.symbol;
+		this.group = data.group;
+		this.name = data.name;
+		this.hexColor = data.hexColor;
+	}
+}
