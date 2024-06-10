6c6a4480b8200e9aece9b1a3b4461b05e70d12c8
lightest <lightest13@gmail.com>
2024-06-10 01:07:20 +0100

    electron configuration parsing moved to ChemicalElement class.

--- src/components/ChemElement/ChemElement.tsx
+++ src/components/ChemElement/ChemElement.tsx
@@ -1,85 +1,11 @@
 import "./ChemElement.css";
-import { iChemElement } from "../../types/iChemElement";
+import { ChemicalElement, iChemElement } from "../../types/iChemElement";
 import { ELEMENT_GROUP_TO_CSS_CLASS } from "../Dataset";
 
 const ROTATION_ANGLE = 15;
 
-function getElemenPositioning(chemEl: iChemElement)
+interface componentProps
 {
-	const electronConfig = chemEl.electronConfiguration;
-
-	// Electron configuration is the distribution of electrons in atomic or molecular orbitals.
-	const bracketIndex = electronConfig.indexOf("]");
-	const outermostElectronShellStr = electronConfig.substring(bracketIndex + 1).trim();
-
-	// TODO: potentially employ regexes for identifying subshells as well.
-	const subshells = outermostElectronShellStr.split(" ");
-	const outerElectronShell = {};
-	let valentElectrons = 0;
-	let period = 0;
-	let group = 0;
-
-	// Even though our dataset presents already sorted subshells, we go extra mile to ensure we always pick the highest.
-	// This will improve our chances of getting the correct value, should the notation in the incoming dataset change.
-	for (let i = 0; i < subshells.length; i++)
-	{
-		const match = subshells[i].match(/(\d+)([spdf]+)(\d+)/);
-		if (match)
-		{
-			// s, p, d, f orbital types.
-			const orbitalType = match[2];
-
-			outerElectronShell[orbitalType] =
-			{
-				n: parseInt(match[1], 10),
-				electrons: parseInt(match[3], 10)
-			};
-
-			// Period of the element is the max quantum number amon outer most shells.
-			period = Math.max(period, outerElectronShell[orbitalType].n);
-
-			if (orbitalType === "s" || orbitalType === "p" || orbitalType === "d")
-			{
-				valentElectrons += outerElectronShell[orbitalType].electrons;
-			}
-		}
-	}
-
-	// Lanthanides and Actinides are a special case. They are placed two rows below the table.
-	// Their actual periods are 6 and 7 respectively.
-	if (chemEl.group === "Lanthanide" || chemEl.group === "Actinide")
-	{
-		period += 2;
-
-		if (outerElectronShell["f"] !== undefined)
-		{
-			valentElectrons += outerElectronShell["f"].electrons;
-		}
-	}
-
-	// TODO: identify noble gas by it's electron configuration.
-	if (chemEl.group === "Noble gas")
-	{
-		group = 18;
-	}
-	else if (outerElectronShell["p"] !== undefined && outerElectronShell["d"] === undefined)
-	{
-		group = valentElectrons + 10;
-	}
-	else
-	{
-		group = valentElectrons;
-	}
-
-	console.log("location for", electronConfig, period, group)
-
-	return {
-		row: period,
-		col: group
-	}
-}
-
-interface componentProps {
 	chemEl: iChemElement,
 	idx: number,
 	className: string,
@@ -88,7 +14,7 @@ interface componentProps {
 
 export default function ChemElement({ chemEl, idx, className, onClick }: componentProps)
 {
-	const { row, col } = getElemenPositioning(chemEl);
+	const el = new ChemicalElement(chemEl);
 
 	function handleMouseMove(e)
 	{
@@ -113,11 +39,12 @@ export default function ChemElement({ chemEl, idx, className, onClick }: compone
 		}
 	}
 
-	console.log("Chem el render", chemEl, row);
+	console.log("Chem el render", chemEl, el.periodNumber);
 
-	const style = {
-		gridRowStart: row,
-		gridColumnStart: col,
+	const style =
+	{
+		gridRowStart: el.periodNumber,
+		gridColumnStart: el.groupNumber,
 		// background: `#${chemEl.hexColor}`
 	};
 
--- src/types/iChemElement.ts
+++ src/types/iChemElement.ts
@@ -9,15 +9,24 @@ export interface iChemElement
 	hexColor: string
 }
 
+interface iElectronShell
+{
+	n: number,
+	electrons: number
+}
+
 export class ChemicalElement
 {
 	atomicNumber: number;
 	atomicMass: number;
 	electronConfiguration: string;
 	symbol: string;
-	group: string;
+	chemicalGroup: string;
 	name: string;
 	hexColor: string;
+	valentElectrons: number;
+	periodNumber: number;
+	groupNumber: number;
 
 	constructor(data: iChemElement)
 	{
@@ -25,8 +34,81 @@ export class ChemicalElement
 		this.atomicMass = data.atomicMass;
 		this.electronConfiguration = data.electronConfiguration;
 		this.symbol = data.symbol;
-		this.group = data.group;
+		this.chemicalGroup = data.group;
 		this.name = data.name;
 		this.hexColor = data.hexColor;
+
+		this.valentElectrons = 0;
+		this.periodNumber = 0;
+		this.groupNumber = 0;
+
+		this.parseElectronConfiguration();
+	}
+
+	parseElectronConfiguration()
+	{
+		const electronConfig = this.electronConfiguration;
+
+		// Electron configuration is the distribution of electrons in atomic or molecular orbitals.
+		const bracketIndex = electronConfig.indexOf("]");
+		const outermostElectronShellStr = electronConfig.substring(bracketIndex + 1).trim();
+
+		// TODO: potentially employ regexes for identifying subshells as well.
+		const subshells = outermostElectronShellStr.split(" ");
+		const outerElectronShell:Record<string, iElectronShell> = {};
+		this.valentElectrons = 0;
+		this.periodNumber = 0;
+
+		// Even though our dataset presents already sorted subshells, we go extra mile to ensure we always pick the highest.
+		// This will improve our chances of getting the correct value, should the notation in the incoming dataset change.
+		for (let i = 0; i < subshells.length; i++)
+		{
+			const match = subshells[i].match(/(\d+)([spdf]+)(\d+)/);
+			if (match)
+			{
+				// s, p, d, f orbital types.
+				const orbitalType = match[2];
+
+				outerElectronShell[orbitalType] =
+				{
+					n: parseInt(match[1], 10),
+					electrons: parseInt(match[3], 10)
+				};
+
+				// Period of the element is the max quantum number amon outer most shells.
+				this.periodNumber = Math.max(this.periodNumber, outerElectronShell[orbitalType].n);
+
+				if (orbitalType === "s" || orbitalType === "p" || orbitalType === "d")
+				{
+					this.valentElectrons += outerElectronShell[orbitalType].electrons;
+				}
+			}
+		}
+
+		// Lanthanides and Actinides are a special case. They are placed two rows below the table.
+		// Their actual periods are 6 and 7 respectively.
+		if (this.chemicalGroup === "Lanthanide" || this.chemicalGroup === "Actinide")
+		{
+			this.periodNumber += 2;
+
+			if (outerElectronShell["f"] !== undefined)
+			{
+				this.valentElectrons += outerElectronShell["f"].electrons;
+			}
+		}
+
+		// TODO: identify noble gas by it's electron configuration.
+		if (this.chemicalGroup === "Noble gas")
+		{
+			this.groupNumber = 18;
+		}
+		else if (outerElectronShell["p"] !== undefined && outerElectronShell["d"] === undefined)
+		{
+			this.groupNumber = this.valentElectrons + 10;
+		}
+		else
+		{
+			this.groupNumber = this.valentElectrons;
+		}
 	}
 }
